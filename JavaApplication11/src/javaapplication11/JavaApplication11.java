8e8fadea6070006c5e98ea6553983c12ed1c9f4b
LAB-USR-AREQUIPA <LAB-USR-AREQUIPA@UTP25269089.grupoutp.net>
2025-08-23 09:36:01 -0500

    se realizo la sincronizacion
    se agreg un codigo para buscar en el arreglo ingresado manualmente

--- JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -4,6 +4,7 @@
  */
 package javaapplication11;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -27,16 +28,49 @@ public class JavaApplication11 {
     }
      public static void main(String[] args) {
         // TODO code application logic here
-         System.out.println("ingrese el numero a buscar en la base");
-        Scanner numero = new Scanner (System.in);
-        int [] arreglo = {10,15,20,30}; 
-        int elemento = numero.nextInt ();
-        boolean encontrar = buscarElemento (arreglo,elemento);
-        System.out.println("El elemento " + elemento );
-        if (encontrar) {
-            System.out.println("si se encuentra");}
-        else {
-            System.out.println("no se encontro");
+        System.out.println("Cuantos Numeros desea ingresar");
+         int cantidad = scanner.nextInt();
+         int[] arreglo = new int[cantidad];
+         
+            System.out.println("Ingrese los " + cantidad + " números:");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            arreglo[i] = scanner.nextInt();
         }
-     }
-     }    
+        System.out.println("\nArreglo original:");
+        System.out.println(Arrays.toString(arreglo));
+        
+        Arrays.sort(arreglo);
+        
+        System.out.println("Arreglo ordenado:");
+        System.out.println(Arrays.toString(arreglo));
+        
+        System.out.println("\nIngrese el número a buscar en la base:");
+        int elemento = scanner.nextInt();
+        
+        boolean encontrar = buscarElemento(arreglo, elemento);
+        
+         System.out.println("\nEl elemento " + elemento);
+        if (encontrar) {
+            System.out.println("Sí se encuentra.");
+        } else {
+            System.out.println("No se encontró.");
+        }
+
+        scanner.close();
+        }
+}
+        
+         //System.out.println("ingrese el numero a buscar en la base");
+        //Scanner numero = new Scanner (System.in);
+        //int [] arreglo = {10,15,20,30}; 
+        //int elemento = numero.nextInt ();
+        //boolean encontrar = buscarElemento (arreglo,elemento);
+        //System.out.println("El elemento " + elemento );
+        //if (encontrar) {
+        //    System.out.println("si se encuentra");}
+       // else {
+        //    System.out.println("no se encontro");
+      //  }
+    // }
+     
